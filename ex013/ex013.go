358ff6ab88b7333dbe75bbca5d68dea5ddd6675c
omaraljawabri <164200540+omaraljawabri@users.noreply.github.com>
2024-04-13 14:45:06 -0300

    att

--- ex013/ex013.go
+++ ex013/ex013.go
@@ -11,14 +11,10 @@ func main(){
 	fmt.Scan(&Nota)
 	if Nota >= 9 {
 		Conceito = "A"
-	} else if Nota > 7.5 {
-		if Nota < 9 {
+	} else if Nota > 7.5 && Nota < 9 {
 			Conceito = "B"	
-		}
-	} else if Nota >= 6 {
-		if Nota <= 7.5 {
+	} else if Nota >= 6 && Nota <= 7.5 {
 			Conceito = "C"	
-		}
 	} else if Nota < 6 {
 		Conceito = "D"
 	}
