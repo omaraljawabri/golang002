ba38b8b048549eaaf825d7a73ff56b6d4d8b49ee
omaraljawabri <164200540+omaraljawabri@users.noreply.github.com>
2024-04-12 23:12:21 -0300

    att ex003

---
+++ ex003/ex003.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main(){
+	var n1, n2, n3 string
+	fmt.Scanln(&n1)
+	fmt.Scanln(&n2)
+	fmt.Scanln(&n3)
+	var z string = n1 + n2 + n3
+	var a, b, c, i int
+	a, err:= strconv.Atoi(n1)
+	b, err = strconv.Atoi(n2)
+	c, err = strconv.Atoi(n3)
+	i, err = strconv.Atoi(z)
+	if err != nil{
+		panic(err)
+	}
+	if a < 10 && b < 10 && c < 10{
+		fmt.Println(z, i*i)
+	}else{
+		fmt.Println("Digito Inválido!")
+	}
+}
